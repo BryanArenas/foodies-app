cb6615e407c8fee719cc031557552e1b97b4bc59
bryangoonzalez <bryangoonzalez@gmail.com>
2020-08-10 21:21:48 -0400

    setup controllers and models

---
+++ app/serializers/restaurant_serializer.rb
@@ -0,0 +1,5 @@
+class RestaurantSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes :name, :slug, :image_url
+  has_many :reviews
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.create([
+    {
+        name: "Chipotle",
+        image_url: "https://upload.wikimedia.org/wikipedia/en/thumb/3/3b/Chipotle_Mexican_Grill_logo.svg/1200px-Chipotle_Mexican_Grill_logo.svg.png"
+    },
+    {
+        name: "Melao Bakery",
+        image_url: "http://www.melaobakery.com/wp-content/uploads/2018/03/melao-logo.png"
+    },
+    {
+        name: "Chili's",
+        image_url: "http://logok.org/wp-content/uploads/2014/10/Chilis-logo-old.png"
+    },
+    {
+        name: "Cheddar's",
+        image_url: "https://www.nrn.com/sites/nrn.com/files/cheddars-logo.gif"
+    },
+    {
+        name: "Miller's Ale House",
+        image_url: "https://cblproperty.blob.core.windows.net/production/assets/blt87587ec9381653c4-millers-ale-house-logo-full-red.png"
+    },
+    {
+        name: "Buffalo Wild Wings",
+        image_url: "http://www.oceansidechamber.com/uploads/4/4/5/3/44535401/buffalo-wild-wings-logo.png"
+    }
+
+])
